fb1e4dcefb9c0fc885fcaf3528e1090aa9551763
IC cheung <qz344@cornell.edu>
2022-08-27 21:44:26 -0400

    check code

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,15 +1,15 @@
-""""
+"""
 Tests for models.
 """
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
 class ModelTests(TestCase):
-    """Test modles."""
+    """Test models."""
 
 
     def test_create_user_with_email_successful(self):
-        """Test creating a user with an email is successfull."""
+        """Test creating a user with an email is successful."""
         email = 'test@example.com'
         password = 'testpass123'
         user = get_user_model().objects.create_user(
