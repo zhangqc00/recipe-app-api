c452b68acbb03c300a6e380c66933d071392eb72
IC cheung <qz344@cornell.edu>
2022-08-28 13:43:02 -0400

    check

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -20,14 +20,21 @@ class AdminSiteTests(TestCase):
         self.client.force_login(self.admin_user)
         self.user = get_user_model().objects.create_user(
             email='user@example.com',
-            password='test[ass123',
+            password='testpass123',
             name='Test User'
         )
 
-    def test_users_list(self):
+    def test_users_lists(self):
         """Test that users are listed on page."""
-        url = reverse('admin:core_user_changlist')
+        url = reverse('admin:core_user_changelist')
         res = self.client.get(url)
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_edit_user_page(self):
+        """Test the edit user page works."""
+        url = reverse('admin:core_user_change', args=[self.user_id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -8,7 +8,6 @@ from django.contrib.auth import get_user_model
 class ModelTests(TestCase):
     """Test models."""
 
-
     def test_create_user_with_email_successful(self):
         """Test creating a user with an email is successful."""
         email = 'test@example.com'
@@ -22,7 +21,7 @@ class ModelTests(TestCase):
         self.assertTrue(user.check_password(password))
 
     def test_new_user_email_normalized(self):
-        """Test email is normailized for new users."""
+        """Test email is normalized for new users."""
         sample_emails = [
             ['test1@EXAMPLE.com', 'test1@example.com'],
             ['Test2@Example.com', 'Test2@example.com'],
